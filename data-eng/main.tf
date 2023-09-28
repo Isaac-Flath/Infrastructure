fdff8c65fa07ed59be387c4316e7f95d3f9a924e
Isaac Flath <Isaac.flath@gmail.com>
2023-09-28 13:56:44 -0400

    Apply redshift cluster

--- data-eng/main.tf
+++ data-eng/main.tf
@@ -29,26 +29,26 @@ resource "aws_s3_bucket" "udacitydataengineeringaws" {
   }
 }
 
-# resource "aws_redshift_cluster" "redshift-cluster-1" {
-#   cluster_identifier  = "redshift-cluster-1"
-#   database_name       = "dev"
-#   iam_roles           = [aws_iam_role.redshift_etl_role.arn, ]
-#   node_type           = "dc2.large"
-#   cluster_type        = "single-node"
+resource "aws_redshift_cluster" "redshift-cluster-1" {
+  cluster_identifier  = "redshift-cluster-1"
+  database_name       = "dev"
+  iam_roles           = [aws_iam_role.redshift_etl_role.arn, ]
+  node_type           = "dc2.large"
+  cluster_type        = "single-node"
+  skip_final_snapshot = true
+  master_password     = var.REDSHIFT_CLUSTER_1_PASS
+  master_username     = "awsuser"
+  port                = 5439
+}
+
+
+# resource "aws_db_instance" "default" {
+#   allocated_storage   = 10
+#   db_name             = "postgres-1"
+#   engine              = "postgres"
+#   instance_class      = "db.t3.micro"
+#   username            = "awsuser"
+#   password            = var.REDSHIFT_CLUSTER_1_PASS
 #   skip_final_snapshot = true
-#   master_password     = var.REDSHIFT_CLUSTER_1_PASS
-#   master_username     = "awsuser"
-#   port                = 5439
 # }
-
-
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-053b0d53c279acc90"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
-
 
