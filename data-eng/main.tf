685c5feec85628b61a5137584066f6e6f168f736
Isaac Flath <Isaac.flath@gmail.com>
2023-09-29 13:27:17 -0400

    redshift networking

--- data-eng/main.tf
+++ data-eng/main.tf
@@ -28,27 +28,3 @@ resource "aws_s3_bucket" "udacitydataengineeringaws" {
     Program = "Udacity Data Engineering Nanodegree"
   }
 }
-
-resource "aws_redshift_cluster" "redshift-cluster-1" {
-  cluster_identifier  = "redshift-cluster-1"
-  database_name       = "dev"
-  iam_roles           = [aws_iam_role.redshift_etl_role.arn, ]
-  node_type           = "dc2.large"
-  cluster_type        = "single-node"
-  skip_final_snapshot = true
-  master_password     = var.REDSHIFT_CLUSTER_1_PASS
-  master_username     = "awsuser"
-  port                = 5439
-}
-
-
-# resource "aws_db_instance" "default" {
-#   allocated_storage   = 10
-#   db_name             = "postgres-1"
-#   engine              = "postgres"
-#   instance_class      = "db.t3.micro"
-#   username            = "awsuser"
-#   password            = var.REDSHIFT_CLUSTER_1_PASS
-#   skip_final_snapshot = true
-# }
-
